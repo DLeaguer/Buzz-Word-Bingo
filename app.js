91f87dd91024d4549cb55831577feedfef6ed1b5
DLeaguer <immostlikely@hotmail.com>
2018-08-26 21:51:16 -1000

    post add buzzword working

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
 const app = express();
+const bp = require('body-parser')
 
 const PORT = process.env.PORT || 8080;
 
+app.use(bp.urlencoded({ extended: true }))
 // make some routes
 
 app.get('/', (req, res) => {
   res.send('HELLO WORLD!')
 })
 
-const nextId = 3;
+let nextId = 3;
 const words = [
   {
     id: 1,
@@ -27,18 +29,33 @@ app.get('/buzzwords', (req, res) => {
   res.json(words)
 })
 
-app.get('/buzzwords/:id', (req, res) => {
+app.get('/buzzwords/:id/', (req, res) => {
   const { id } = req.params
   console.log('id =', id)
   const resData = words.filter(item => {
-    console.log('item.id, id =', item.id, id)
-    return id == item.id
+    console.log('item.id =', item.id)
+    // console.log('id =', id)
+    // return id == item.id
+    return item.id == id
   })
   console.log('resData =', resData)
-  res.json(resData)
+  res.json(resData[0])
 })
 
-app.post('/addbuzzword', (req, res) => {})
+app.post('/addbuzzword', (req, res) => {
+  console.log('req.body =', req.body)
+  const wordObject = req.body
+  console.log('wordObject =', wordObject)
+  wordObject.id = nextId
+  console.log('wordObject.id =', wordObject.id)
+  words.push(wordObject)
+  console.log('words =', words)
+  res.redirect(`/buzzwords/${nextId}/`)
+  console.log('what now!!!')
+  nextId++
+  console.log('did this work nexId =', nextId)
+  // res.sendStatus(200)
+})
 
 app.listen(PORT, () => {
   console.log(`Server has started on port: ${PORT}`)
