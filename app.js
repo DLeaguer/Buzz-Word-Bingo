aebdb807715c2c06c1119374fe0100fbd7158536
DLeaguer <immostlikely@hotmail.com>
2018-08-28 10:58:17 -1000

    added post /heard, updated post /reset with if/else statements

--- app.js
+++ app.js
@@ -1,25 +1,26 @@
-const express = require('express');
-const app = express();
+const express = require('express')
+const app = express()
 const bp = require('body-parser')
 
-const PORT = process.env.PORT || 8080;
+const PORT = process.env.PORT || 8080
+
+
+
+// ** MIDDLEWARE **
 
 app.use(bp.urlencoded({ extended: true }))
 
 app.use((req, res, next) => {
-  console.log(`\n${req.method} request at: ${req.url}`);
-  next();
+  console.log(`\n${req.method} request at: ${req.url}`)
+  next()
 });
 
+// ** TO GET ALL USE OF PUBLIC FOLDER **
 app.use(express.static('public'))
 
-// app.get("/", (req, res) => {
-//   res.sendFile(__dirname + "/public/index.html")
-// })
 
-// app.get('/css/styles.css', (req, res) => {
-//   res.sendFile(__dirname + '/public/css/styles.css')
-// })
+
+// ** ROUTES  **
 
 let Objects = []
 let words = []
@@ -27,8 +28,8 @@ let totalScore = 0
 
 app.get('/buzzwords', (req, res) => {
   res.json(Objects)
-  console.log('Objects =')
-  console.log(Objects);
+  // console.log('Objects =')
+  // console.log(Objects);
 })
 
 app.post('/buzzwords', (req, res) => {
@@ -39,12 +40,12 @@ app.post('/buzzwords', (req, res) => {
   // console.log('words', words)
   // console.log('Objects.length before =', Objects.length)
   if (words.includes(wordObj.buzzWord)) {
-    console.log('  **buzzword already posted')
-    res.send({ success: false})
+    console.log('  ** buzzword already posted')
+    res.send({ "success": false})
   } 
   else if (Objects.length > 1) {
-    console.log('  **limit of buzzwords reached')
-    res.send({ 'success': false})
+    console.log('  ** limit of buzzwords reached')
+    res.send({ "success": false})
   }
   else {
     console.log('wordObj.buzzWord =', wordObj.buzzWord)
@@ -60,12 +61,19 @@ app.post('/buzzwords', (req, res) => {
 })
 
 app.post('/reset', (req, res) => {
-  const reset = req.body
-  console.log('reset =', reset)
-  Objects = []
-  words = []
-  totalScore = 0
-  res.send({ 'success': true })
+  const resetObj = req.body
+  console.log('resetObj =', resetObj)
+  console.log('resetObj.reset =', resetObj.reset)
+  if (resetObj.reset = true) {
+    Objects = []
+    words = []
+    totalScore = 0
+    res.send({ "success": true })
+  }
+  else {
+    res.send({ "success": false})
+    console.log('COULD NOT reset')
+  }
 })
 
 app.delete('/buzzwords', (req, res) => {
@@ -75,14 +83,37 @@ app.delete('/buzzwords', (req, res) => {
     Objects = Objects.filter(element => {
         return element.buzzWord !== wordObj.buzzWord
     })
-    res.send({ success: true})
+    words = words.filter(element => {
+      return element !== wordObj.buzzWord
+    })
+    res.send({ "success": true})
     console.log('buzzword DELETED')
   }
   else {
-    res.send({ success: false})
-    console.log('buzzword NOT deleted')
+    res.send({ "success": false})
+    console.log('buzzword COULD NOT BE deleted')
   }  
 })
+
+app.post('/heard', (req, res) => {
+  const heard = req.body
+  console.log('heard =', heard)
+  if (words.includes(heard.buzzWord)) {
+    Objects.filter(element => {
+      console.log('element =', element)
+      if (element.buzzWord === heard.buzzWord)
+      totalScore += Number(element.points)
+    })
+    res.send( `{ "totalScore": ${totalScore} }`)
+    console.log('totalScore =', totalScore)
+  }
+  else {
+    res.send({ "success": false })
+    console.log('  ** buzzword NOT FOUND')
+  }
+})
+
+
 
 app.listen(PORT, () => {
   console.log(`Server has started on port: ${PORT}`)
