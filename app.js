e141fbc95542756b1e21a4ee017590be8f4d6bb1
DLeaguer <immostlikely@hotmail.com>
2018-08-28 04:44:18 -1000

    added post /reset, delete /buzzwords, if/else statements for post /buzzwords, and use express.static folder

--- app.js
+++ app.js
@@ -5,56 +5,83 @@ const bp = require('body-parser')
 const PORT = process.env.PORT || 8080;
 
 app.use(bp.urlencoded({ extended: true }))
-// make some routes
 
-app.get('/', (req, res) => {
-  res.send('HELLO WORLD!')
-})
+app.use((req, res, next) => {
+  console.log(`\n${req.method} request at: ${req.url}`);
+  next();
+});
 
-let nextId = 3;
-const words = [
-  {
-    id: 1,
-    buzzWord: 'Agile is amazing',
-    points: '1000'
-  },
-  {
-    id: 2,
-    buzzWord: 'Social-Mobile',
-    points: '100'
-  }
-]
+app.use(express.static('public'))
+
+// app.get("/", (req, res) => {
+//   res.sendFile(__dirname + "/public/index.html")
+// })
+
+// app.get('/css/styles.css', (req, res) => {
+//   res.sendFile(__dirname + '/public/css/styles.css')
+// })
+
+let Objects = []
+let words = []
+let totalScore = 0
 
 app.get('/buzzwords', (req, res) => {
-  res.json(words)
+  res.json(Objects)
+  console.log('Objects =')
+  console.log(Objects);
 })
 
-app.get('/buzzwords/:id/', (req, res) => {
-  const { id } = req.params
-  console.log('id =', id)
-  const resData = words.filter(item => {
-    console.log('item.id =', item.id)
-    // console.log('id =', id)
-    // return id == item.id
-    return item.id == id
-  })
-  console.log('resData =', resData)
-  res.json(resData[0])
+app.post('/buzzwords', (req, res) => {
+  // console.log('req.body =', req.body)
+  const wordObj = req.body
+  // console.log('wordObj =', wordObj)
+  // console.log('wordObj.buzzWord =', wordObj.buzzWord)
+  // console.log('words', words)
+  // console.log('Objects.length before =', Objects.length)
+  if (words.includes(wordObj.buzzWord)) {
+    console.log('  **buzzword already posted')
+    res.send({ success: false})
+  } 
+  else if (Objects.length > 1) {
+    console.log('  **limit of buzzwords reached')
+    res.send({ 'success': false})
+  }
+  else {
+    console.log('wordObj.buzzWord =', wordObj.buzzWord)
+    words.push(wordObj.buzzWord)
+    Objects.push(wordObj)
+    console.log('Objects =')
+    console.log(Objects)
+    console.log('words =', words)
+    console.log('Objects.length after post =', Objects.length)
+    res.send({ "success": true})
+    console.log('successful post what now!!!')
+  }
 })
 
-app.post('/addbuzzword', (req, res) => {
-  console.log('req.body =', req.body)
-  const wordObject = req.body
-  console.log('wordObject =', wordObject)
-  wordObject.id = nextId
-  console.log('wordObject.id =', wordObject.id)
-  words.push(wordObject)
-  console.log('words =', words)
-  res.redirect(`/buzzwords/${nextId}/`)
-  console.log('what now!!!')
-  nextId++
-  console.log('did this work nexId =', nextId)
-  // res.sendStatus(200)
+app.post('/reset', (req, res) => {
+  const reset = req.body
+  console.log('reset =', reset)
+  Objects = []
+  words = []
+  totalScore = 0
+  res.send({ 'success': true })
+})
+
+app.delete('/buzzwords', (req, res) => {
+  const wordObj = req.body
+  console.log('wordObj =', wordObj)
+  if (words.includes(wordObj.buzzWord)) {
+    Objects = Objects.filter(element => {
+        return element.buzzWord !== wordObj.buzzWord
+    })
+    res.send({ success: true})
+    console.log('buzzword DELETED')
+  }
+  else {
+    res.send({ success: false})
+    console.log('buzzword NOT deleted')
+  }  
 })
 
 app.listen(PORT, () => {
